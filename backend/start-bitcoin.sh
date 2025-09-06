e8138904f5c41965ddcc121717670e49b87a406c
Pedro Prete <pmprete@gmail.com>
2025-09-06 10:10:42 -0300

    Add btc explorer

--- backend/start-bitcoin.sh
+++ backend/start-bitcoin.sh
@@ -2,14 +2,30 @@
 set -e
 
 # stop and remove the bitcoin-regtest container if it exists
-CONTAINER_NAME=bitcoin-regtest
-if [ -n "$(docker ps -a -f name="^$CONTAINER_NAME$" -q)" ]; then
-    echo "Stopping and remove $CONTAINER_NAME container"
-    docker rm -f $CONTAINER_NAME
+BITCOIND_NAME=bitcoin-regtest
+if [ -n "$(docker ps -a -f name="^$BITCOIND_NAME$" -q)" ]; then
+    echo "Stopping and remove $BITCOIND_NAME container"
+    docker rm -f $BITCOIND_NAME
 fi
 
-# Start the Bitcoin Regtest node
-docker run --name $CONTAINER_NAME -d -p 18443:18443 -e BITCOIN_DATA=/data ruimarinho/bitcoin-core \
+# stop and remove the bitcoin-regtest container if it exists
+EXPLORER_NAME=btc-rpc-explorer
+if [ -n "$(docker ps -a -f name="^$EXPLORER_NAME$" -q)" ]; then
+    echo "Stopping and remove $EXPLORER_NAME container"
+    docker rm -f $EXPLORER_NAME
+fi
+
+# Create a network for the containers
+if ! docker network inspect bitcoin-net >/dev/null 2>&1; then
+  docker network create bitcoin-net
+fi
+
+# Start the Bitcoin Regtest node https://hub.docker.com/r/ruimarinho/bitcoin-core/dockerfile
+docker run --name $BITCOIND_NAME -d \
+    --network bitcoin-net \
+    -p 18443:18443 \
+    -e BITCOIN_DATA=/data \
+    ruimarinho/bitcoin-core \
         -regtest=1 \
         -printtoconsole \
         -rpcallowip=0.0.0.0/0 \
@@ -20,7 +36,26 @@ docker run --name $CONTAINER_NAME -d -p 18443:18443 -e BITCOIN_DATA=/data ruimar
         -txindex=1 \
         -fallbackfee=0.0002
 
+# Wait for the container to start
+sleep 2
+
+
+# Start the Bitcoin RPC Explorer https://hub.docker.com/r/tyzbit/btc-rpc-explorer
+docker run  --name $EXPLORER_NAME -d \
+    --network bitcoin-net \
+    -p 4000:4000 \
+    --platform linux/amd64 \
+    -e BTCEXP_PORT=4000 \
+    -e BTCEXP_HOST=0.0.0.0  \
+    -e BTCEXP_BITCOIND_HOST=$BITCOIND_NAME \
+    -e BTCEXP_BITCOIND_PORT=18443 \
+    -e BTCEXP_BITCOIND_USER=foo \
+    -e BTCEXP_BITCOIND_PASS=rpcpassword \
+    tyzbit/btc-rpc-explorer
+
+# Wait for the container to start
 sleep 1
 
-docker exec -it $CONTAINER_NAME sh -c \
+# Start auto mine blocks one per second
+docker exec -it $BITCOIND_NAME sh -c \
 'bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword createwallet "default" && ADDRESS=$(bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword getnewaddress) && while true; do bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword generatetoaddress 1 $ADDRESS && echo "⛏️  Bloque minado" && sleep 1; done'
