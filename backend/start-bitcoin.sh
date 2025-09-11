403c26587776ba0e50b2ec56737c4c022e6d6c6b
Pedro Prete <pmprete@gmail.com>
2025-09-11 11:29:52 -0300

    Use variable for automine blocks time

--- backend/start-bitcoin.sh
+++ backend/start-bitcoin.sh
@@ -58,5 +58,6 @@ docker run  --name $EXPLORER_NAME -d \
 sleep 3
 
 # Start auto mine 1 block per 5 seconds
+BLOCKS_TIME=10
 docker exec -it $BITCOIND_NAME sh -c \
-'bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword createwallet "default" && ADDRESS=$(bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword getnewaddress) && while true; do bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword generatetoaddress 1 $ADDRESS && echo "⛏️  Block mined" && sleep 5; done'
+'bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword createwallet "default" && ADDRESS=$(bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword getnewaddress) && while true; do bitcoin-cli -regtest -rpcuser=foo -rpcpassword=rpcpassword generatetoaddress 1 $ADDRESS && echo "⛏️  Block mined" && sleep '$BLOCKS_TIME'; done'
