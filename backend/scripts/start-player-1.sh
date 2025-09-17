3a964da5d945052ef260cceee4c4bae766225479
Pedro Prete <pmprete@gmail.com>
2025-09-17 19:12:48 -0300

    Fix scripts to avoid buffering

--- backend/scripts/start-player-1.sh
+++ backend/scripts/start-player-1.sh
@@ -6,7 +6,7 @@ CURRENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$CURRENT_PATH/../";
 
 # remove the log file (no need to create the directory its done by the program)
-rm -rf "$CURRENT_PATH/../logs/player_1.log/backend"
+rm -rf "$CURRENT_PATH/../logs/player_1/backend.log"
 
 # run the backend
 RUST_LOG=debug CONFIG_FILE=player_1 RUST_BACKTRACE=1 cargo run
