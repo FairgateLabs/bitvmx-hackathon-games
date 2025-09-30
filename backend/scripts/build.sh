1c33a8a38f97fd0d05f8e322e48f3675b0a558d6
Ignacio <ignacio.pulice@fairgate.io>
2025-09-30 17:58:08 -0300

    Printing info when building

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -5,17 +5,25 @@ set -e
 CURRENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")/.." ; pwd -P )
 cd "$CURRENT_PATH"
 
-# build the backend
-cargo build
-
+echo "🔨 Building the job dispatcher..."
 # build the job dispatcher
 cd "$CURRENT_PATH/dependencies/rust-bitvmx-job-dispatcher"
 cargo build --release
+echo "✅ Job dispatcher built successfully!"
 
+echo "🔨 Building the BitVMX CPU..."
 # build the BitVMX CPU
 cd "$CURRENT_PATH/dependencies/BitVMX-CPU"
 cargo build --release
+echo "✅ BitVMX CPU built successfully!"
 
+echo "🔨 Building the bitvmx client..."
 # build the bitvmx client
 cd "$CURRENT_PATH/dependencies/rust-bitvmx-client"
 cargo build --release
+echo "✅ BitVMX client built successfully!"
+
+echo "🔨 Building the backend..."
+# build the backend
+cargo build
+echo "✅ Backend built successfully!"
